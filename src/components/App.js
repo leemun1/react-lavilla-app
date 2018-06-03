73956a32cac9cc4cf44d582221759ec7bddf891e
Mike <munkyu0813@gmail.com>
2018-06-03 17:47:19 -0400

    construct basic layout

--- src/components/App.js
+++ src/components/App.js
@@ -3,8 +3,16 @@ import React, { Component } from "react";
 class App extends Component {
   render() {
     return (
-      <div>
-        <h1>app</h1>
+      <div className="container">
+        <div className="sidebar">Sidebar</div>
+        <header className="header">Header</header>
+        <div className="realtors">Meet the team</div>
+        <section className="features">Features</section>
+        <div className="story__pictures">story pictures</div>
+        <div className="story__content">Story content</div>
+        <section className="homes">homes</section>
+        <section className="gallery">gallery</section>
+        <footer className="footer">footer</footer>
       </div>
     );
   }
