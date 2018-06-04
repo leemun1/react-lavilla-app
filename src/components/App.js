0e7c351e4a0d2d955071f89e9885e73347a8e548
Mike <munkyu0813@gmail.com>
2018-06-03 20:17:46 -0400

    add markup and style for story section

--- src/components/App.js
+++ src/components/App.js
@@ -78,9 +78,20 @@ class App extends Component {
             </p>
           </div>
         </section>
-        <div className="story__pictures">story pictures</div>
-        <div className="story__content">Story content</div>
-        <section className="homes">homes</section>
+        <div className="story__picture" />
+        <div className="story__content">
+          <h3 className="heading-3 mb-sm">Happy customers</h3>
+          <h2 className="heading-2 heading-2--dark mb-md">
+            &ldquo;The best decision for our family&rdquo;
+          </h2>
+          <p className="story__text">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tenetur
+            distinctio necessitatibus pariatur voluptatibus. Quidem consequatur
+            harum volupta!
+          </p>
+          <button className="button">Find your new home</button>
+        </div>
+        <section className="homes" />
         <section className="gallery">gallery</section>
         <footer className="footer">footer</footer>
       </div>
